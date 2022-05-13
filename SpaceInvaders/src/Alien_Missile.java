bd5bcd3c3d2b6060dc5134eba7d0d4e6978de11d
SJChang123 <shenrunz888@gmail.com>
2022-05-12 22:44:11 -0400

    Added Alien and Alien_Missile classes

--- SpaceInvaders/src/Alien_Missile.java
+++ SpaceInvaders/src/Alien_Missile.java
@@ -1,4 +1,21 @@
-
-public class Alien_Missile {
-
+public class Alien_Missile extends Sprite{
+	private final int BOARD_WIDTH = 390;
+    private final int MISSILE_SPEED = 2;
+    
+    public Alien_Missile(int x, int y){
+        super(x, y);
+        initMissile();
+    }
+    private void initMissile(){
+        loadImage("resources/alien_missile.png");
+        getImageDimensions();
+    }
+    public void move(){
+    
+        y -= MISSILE_SPEED;
+        
+        if (y > BOARD_WIDTH){
+            visible = false;
+        }
+    }
 }
